0ebe2951e6a1ac2a442216518cd7d5e370441543
Nacho <iaacosta@uc.cl>
2020-02-25 16:02:01 -0300

    chore(backend): changes orm config to accept environment variables
    for testing purposes

---
+++ backend/ormconfig.js
@@ -0,0 +1,12 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: 5432,
+  synchronize: false,
+  logging: ['query', 'error'],
+  entities: ['./src/models/**/*.ts'],
+  migrations: ['./src/migrations/**/*.ts'],
+};
