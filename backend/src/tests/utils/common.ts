fb6af4bd7e1eabf11eb1279d47fe3d039d7cb9bf
Nacho <iaacosta@uc.cl>
2020-02-27 15:01:25 -0300

    test(backend): adds a nested query test case for category and currency api calls

--- backend/src/tests/utils/common.ts
+++ backend/src/tests/utils/common.ts
@@ -10,17 +10,42 @@ import {
 export const getCurrencyById = (_id: number) =>
   currencies.find(({ id }) => id === _id);
 
-export const getDebitAccountById = (_id: number) =>
-  debitAccounts.find(({ id }) => id === _id);
-
-export const getCreditAccountById = (_id: number) =>
-  creditAccounts.find(({ id }) => id === _id);
-
 export const getCategoryById = (_id: number) => {
   const category = categories.find(({ id }) => id === _id)!;
   delete (category as any).createdAt;
   delete (category as any).updatedAt;
   return category;
+};
+
+export const getDebitAccountsRelatedToCurrency = (_currencyId: number) => {
+  const accounts = debitAccounts.filter(
+    ({ currencyId }) => currencyId === _currencyId,
+  );
+
+  return accounts.map(({ id, name, bank, allowsNegative, initialBalance }) => ({
+    id: id.toString(),
+    name,
+    bank,
+    initialBalance,
+    allowsNegative,
+  }));
+};
+
+export const getCreditAccountsRelatedToCurrency = (_currencyId: number) => {
+  const accounts = creditAccounts.filter(
+    ({ currencyId }) => currencyId === _currencyId,
+  );
+
+  return accounts.map(
+    ({ id, name, bank, initialBalance, billingDay, paymentDay }) => ({
+      id: id.toString(),
+      name,
+      bank,
+      initialBalance,
+      billingDay,
+      paymentDay,
+    }),
+  );
 };
 
 export const getSubCategoriesRelatedToCategory = (_categoryId: number) => {
