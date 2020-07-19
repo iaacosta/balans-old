09db42e9af764dbaeaed8ce67387c30dc1e7e8bd
Nacho <iaacosta@uc.cl>
2020-07-19 01:37:42 -0400

    feat(authentication): adds base redux flow to use tokens

--- web/src/config/apollo.ts
+++ web/src/config/apollo.ts
@@ -1,10 +1,18 @@
 /* eslint-disable no-console */
 import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
 import { capitalize } from 'lodash';
+import { baseStore } from './redux';
 
 const baseLink = new HttpLink({
   uri: process.env.API_URL || 'http://localhost:5000/graphql',
+});
+
+const contextLink = setContext((_, { headers }) => {
+  const { token } = baseStore.getState();
+  if (token) return { headers: { ...headers, Authorization: `Bearer ${token}` } };
+  return { headers };
 });
 
 const errorLink = onError(({ graphQLErrors }) => {
@@ -16,7 +24,7 @@ const errorLink = onError(({ graphQLErrors }) => {
 });
 
 const client = new ApolloClient({
-  link: ApolloLink.from([errorLink, baseLink]),
+  link: ApolloLink.from([contextLink, errorLink, baseLink]),
   cache: new InMemoryCache(),
 });
 
