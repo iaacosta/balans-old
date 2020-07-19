f8c16cd8085dd5074aebe62dbb4244509823af8f
Nacho <iaacosta@uc.cl>
2020-07-19 11:25:04 -0400

    feat(authentication): adds base authenticated app

---
+++ web/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from '@material-ui/core';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+      <Typography variant="h1">Dashboard</Typography>
+    </Box>
+  );
+};
+
+export default Dashboard;
