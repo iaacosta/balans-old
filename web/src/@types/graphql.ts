24b11bc7b29c43972379ae6e8e568572aafdf30f
Nacho <iaacosta@uc.cl>
2020-07-22 02:04:21 -0400

    fix(graphql-codegen): enhances use of plugins

---
+++ web/src/@types/graphql.ts
@@ -0,0 +1,138 @@
+export type Maybe<T> = T | null;
+export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
+/** All built-in and custom scalars, mapped to their actual values */
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
+
+export type User = {
+  __typename?: 'User';
+  id: Scalars['ID'];
+  name: Scalars['String'];
+  email: Scalars['String'];
+  username: Scalars['String'];
+  role: Scalars['String'];
+};
+
+export type ByIdInput = {
+  id: Scalars['ID'];
+};
+
+export type CreateUserInput = {
+  firstName: Scalars['String'];
+  lastName: Scalars['String'];
+  username: Scalars['String'];
+  email: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type UpdateUserInput = {
+  firstName?: Maybe<Scalars['String']>;
+  lastName?: Maybe<Scalars['String']>;
+  email?: Maybe<Scalars['String']>;
+  password?: Maybe<Scalars['String']>;
+  currentPassword: Scalars['String'];
+};
+
+export type UpdateAnyUserInput = {
+  id: Scalars['ID'];
+  firstName?: Maybe<Scalars['String']>;
+  lastName?: Maybe<Scalars['String']>;
+  email?: Maybe<Scalars['String']>;
+  password?: Maybe<Scalars['String']>;
+  role?: Maybe<Scalars['String']>;
+};
+
+export type LoginInput = {
+  username: Scalars['String'];
+  password: Scalars['String'];
+};
+
+export type Query = {
+  __typename?: 'Query';
+  users: Array<User>;
+  user: User;
+  me: User;
+};
+
+
+export type QueryUserArgs = {
+  input: ByIdInput;
+};
+
+export type Mutation = {
+  __typename?: 'Mutation';
+  login: Scalars['String'];
+  signUp: Scalars['String'];
+  resetDatabase: Scalars['Boolean'];
+  createUser: User;
+  updateUser: User;
+  updateMe: User;
+  deleteUser: Scalars['ID'];
+};
+
+
+export type MutationLoginArgs = {
+  input: LoginInput;
+};
+
+
+export type MutationSignUpArgs = {
+  input: CreateUserInput;
+};
+
+
+export type MutationCreateUserArgs = {
+  input: CreateUserInput;
+};
+
+
+export type MutationUpdateUserArgs = {
+  input: UpdateAnyUserInput;
+};
+
+
+export type MutationUpdateMeArgs = {
+  input: UpdateUserInput;
+};
+
+
+export type MutationDeleteUserArgs = {
+  input: ByIdInput;
+};
+
+export type LoginMutationVariables = Exact<{
+  username: Scalars['String'];
+  password: Scalars['String'];
+}>;
+
+
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & { token: Mutation['login'] }
+);
+
+export type SignUpMutationVariables = Exact<{
+  input: CreateUserInput;
+}>;
+
+
+export type SignUpMutation = (
+  { __typename?: 'Mutation' }
+  & { token: Mutation['signUp'] }
+);
+
+export type MeQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type MeQuery = (
+  { __typename?: 'Query' }
+  & { user: (
+    { __typename?: 'User' }
+    & Pick<User, 'id' | 'name' | 'username' | 'role'>
+  ) }
+);
