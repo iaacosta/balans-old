42c507e793392049290664c45bab04940cc5e6e9
Nacho <iaacosta@uc.cl>
2020-08-06 19:56:25 -0400

    chore(docker-backend): setups better files for development and production environments

--- backend/scripts/docker-build.sh
+++
@@ -1,8 +0,0 @@
-docker build \
-  --tag=nacho19/balans \
-  --build-arg SECRET=$SECRET \
-  --build-arg DB_HOSTNAME=$RDS_HOSTNAME \
-  --build-arg DB_NAME=$RDS_NAME \
-  --build-arg DB_USERNAME=$RDS_USERNAME \
-  --build-arg DB_PASSWORD=$RDS_PASSWORD \
-  .
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,12 +9,16 @@ app.get('/', (req, res) => res.send('Cy.ok').status(200));
 
 /* istanbul ignore next */
 if (NODE_ENV !== 'test') {
-  createConnection().then(async (connection) => {
-    console.log('TypeORM connected to Postgres database');
-    const server = await mountApollo(connection);
-    server.applyMiddleware({ app });
-    app.listen(PORT, () => console.log(`Express app running on port ${PORT}`));
-  });
+  createConnection()
+    .then(async (connection) => {
+      console.log('TypeORM connected to Postgres database');
+      const server = await mountApollo(connection);
+      server.applyMiddleware({ app });
+      app.listen(PORT, () =>
+        console.log(`Express app running on port ${PORT}`),
+      );
+    })
+    .catch((err) => console.error(err));
 }
 
 export default app;
