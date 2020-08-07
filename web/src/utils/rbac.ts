cf06946821314e50ccd9d16c9865adac0cd6a4d8
Nacho <iaacosta@uc.cl>
2020-08-07 01:17:01 -0400

    feat(rbac): adds rbac hook and base rules

---
+++ web/src/utils/rbac.ts
@@ -0,0 +1,56 @@
+import { User } from '../@types/graphql';
+
+export const roles = {
+  USER: 'user',
+  ADMIN: 'admin',
+};
+
+export const actions = {
+  routes: {
+    dashboard: 'routes:dashboard',
+    movements: 'routes:movements',
+    otherMovements: 'routes:otherMovements',
+    places: 'routes:places',
+    people: 'routes:people',
+    users: 'routes:users',
+  },
+  users: {
+    getAll: 'users:getAll',
+    getOne: 'users:getOne',
+    create: 'users:create',
+    edit: 'users:edit',
+    delete: 'users:delete',
+  },
+} as const;
+
+export type Action =
+  | typeof actions['users'][keyof typeof actions['users']]
+  | typeof actions['routes'][keyof typeof actions['routes']];
+
+export const rules = {
+  user: new Set<string>([
+    actions.routes.dashboard,
+    actions.routes.movements,
+    actions.routes.otherMovements,
+    actions.routes.people,
+    actions.routes.places,
+  ]),
+  admin: new Set<string>([
+    actions.routes.dashboard,
+    actions.routes.movements,
+    actions.routes.otherMovements,
+    actions.routes.people,
+    actions.routes.places,
+    actions.routes.users,
+    actions.users.getAll,
+    actions.users.getOne,
+    actions.users.create,
+    actions.users.edit,
+    actions.users.delete,
+  ]),
+};
+
+export const canPerform = (user: Pick<User, 'role'>, action: Action): boolean => {
+  if (rules[user.role as keyof typeof rules].has(action)) return true;
+  return false;
+};
