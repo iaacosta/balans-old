4bdef85a6f9efcbb8218c3a7cc1f56dcda3d1ab2
Nacho <iaacosta@uc.cl>
2020-08-08 16:14:31 -0400

    refactor(cypress): use fixtures + enhance setup database query

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,14 +1,17 @@
-const basePath = process.env.NODE_ENV === 'production' ? './dist' : './src';
+const { NODE_ENV } = process.env;
+const basePath = NODE_ENV === 'production' ? './dist' : './src';
 
 module.exports = {
   type: 'postgres',
   host: process.env.DB_HOSTNAME || 'localhost',
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: ['cypress', 'test'].includes(NODE_ENV)
+    ? `${process.env.DB_NAME}_test`
+    : process.env.DB_NAME,
   port: process.env.DB_PORT || 5432,
   synchronize: false,
-  logging: process.env.NODE_ENV === 'test' ? false : ['query', 'error'],
+  logging: NODE_ENV === 'test' ? false : ['query', 'error'],
   entities: [`${basePath}/models/**/*`],
   migrations: [`${basePath}/migrations/**/*`],
   subscribers: [`${basePath}/subscribers/**/*`],
