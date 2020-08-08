415767c536d7d85ef64e18c83e0267c1a5e9d579
Nacho <iaacosta@uc.cl>
2020-08-08 13:02:03 -0400

    improvement(hooks): adds set prop to useToggleable

--- web/src/hooks/useToggleable.ts
+++ web/src/hooks/useToggleable.ts
@@ -1,10 +1,14 @@
-/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { useState } from 'react';
 
 type Props = { initialToggled?: boolean };
+type Returns = {
+  toggled: boolean;
+  toggle: () => void;
+  set: (value: boolean) => void;
+};
 
-export const useToggleable = ({ initialToggled = false }: Props = {}) => {
+export const useToggleable = ({ initialToggled = false }: Props = {}): Returns => {
   const [toggled, setToggled] = useState(initialToggled);
   const toggle = () => setToggled(!toggled);
-  return { toggled, toggle };
+  return { toggled, toggle, set: setToggled };
 };
