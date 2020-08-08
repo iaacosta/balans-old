149e8f3101c9a2b4456588040a43f22b51250b41
Nacho <iaacosta@uc.cl>
2020-08-07 23:58:53 -0400

    wip(users): add base page and tab basic navigation

---
+++ web/src/pages/Users.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Typography, Tabs, Tab, makeStyles, Paper } from '@material-ui/core';
+import { useTabs } from '../hooks/useTabs';
+
+const useStyles = makeStyles((theme) => ({
+  title: { marginBottom: theme.spacing(2) },
+  paper: { display: 'inline-block' },
+}));
+
+const Users: React.FC = () => {
+  const classes = useStyles();
+  const { selected, change } = useTabs({
+    tabs: ['active', 'deleted'] as const,
+    initialTab: 'active',
+  });
+
+  return (
+    <>
+      <Typography className={classes.title} variant="h5">
+        Platform users
+      </Typography>
+      <Paper className={classes.paper} elevation={1}>
+        <Tabs
+          textColor="secondary"
+          value={selected}
+          onChange={(event, value) => change(value as 'active' | 'deleted')}
+        >
+          <Tab value="active" label="Active users" />
+          <Tab value="deleted" label="Deleted users" />
+        </Tabs>
+      </Paper>
+    </>
+  );
+};
+
+export default Users;
