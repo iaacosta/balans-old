bc2892dfd4d6ad4973cbaef8ce792b9befcd30f7
Nacho <iaacosta@uc.cl>
2020-08-08 15:05:35 -0400

    wip(users): adds pagination to enhanced table

--- web/src/react-app-env.d.ts
+++ web/src/react-app-env.d.ts
@@ -1,1 +1,22 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-empty-interface */
+/* eslint-disable @typescript-eslint/ban-types */
 /// <reference types="react-scripts" />
+import { UsePaginationInstanceProps, UsePaginationOptions, UsePaginationState } from 'react-table';
+
+type Object = Record<string, unknown>;
+declare module 'react-table' {
+  // take this file as-is, or comment out the sections that don't apply to your plugin configuration
+
+  export interface TableOptions<D extends Object>
+    extends UsePaginationOptions<D>,
+      Record<string, any> {}
+
+  export interface TableInstance<D extends Object = {}> extends UsePaginationInstanceProps<D> {}
+  export interface TableState<D extends Object = {}> extends UsePaginationState<D> {}
+
+  export interface Column<D extends Object = {}> {}
+  export interface ColumnInstance<D extends Object = {}> {}
+  export interface Cell<D extends Object = {}> {}
+  export interface Row<D extends Object = {}> {}
+}
