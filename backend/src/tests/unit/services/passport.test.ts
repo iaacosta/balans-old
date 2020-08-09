3c4b9b612449e946815e9c1bf97f7853a225cc1b
Nacho <iaacosta@uc.cl>
2020-08-09 02:18:47 -0400

    test(auth): adapt tests to new payload

--- backend/src/tests/unit/services/passport.test.ts
+++ backend/src/tests/unit/services/passport.test.ts
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
-import * as typeorm from 'typeorm';
 
 import { authenticateUser, getUserFromToken } from '../../../services/passport';
 import { buildUser } from '../../factory/userFactory';
+import TokenExpiredError from '../../../errors/TokenExpiredError';
 
 const foundUser = buildUser();
 const token = 'mockToken';
@@ -11,65 +11,38 @@ describe('passport', () => {
   let verify: jest.SpyInstance;
 
   beforeEach(() => {
-    verify = jest.spyOn(jwt, 'verify').mockImplementation(() => ({ id: 1 }));
+    verify = jest
+      .spyOn(jwt, 'verify')
+      .mockImplementation(() => ({ user: foundUser }));
   });
 
   describe('getUserFromToken', () => {
-    it('should call verify', async () => {
-      await getUserFromToken(token, () => null);
+    it('should call verify', () => {
+      getUserFromToken(token, () => null);
       expect(verify).toHaveBeenCalledTimes(1);
       expect(verify).toHaveBeenCalledWith(token, expect.anything());
     });
 
-    it('should call done with found user', async () => {
-      const findOne = jest.fn(() => foundUser);
-      jest
-        .spyOn(typeorm, 'getRepository')
-        .mockImplementation(() => ({ findOne } as any));
+    it('should call done with found user', () => {
       const done = jest.fn();
-      await getUserFromToken(token, done);
+      getUserFromToken(token, done);
       expect(done).toHaveBeenCalledTimes(1);
       expect(done).toHaveBeenCalledWith(null, foundUser);
     });
 
-    it('should call done with not found user', async () => {
-      const findOne = jest.fn(() => null);
-      jest
-        .spyOn(typeorm, 'getRepository')
-        .mockImplementation(() => ({ findOne } as any));
-
-      const done = jest.fn();
-      await getUserFromToken(token, done);
-      expect(done).toHaveBeenCalledTimes(1);
-      expect(done).toHaveBeenCalledWith(null, null);
-    });
-
     it('should call done with error', async () => {
-      const error = new Error('noop');
-      jest.spyOn(typeorm, 'getRepository').mockImplementation(
-        () =>
-          ({
-            findOne: async () => {
-              throw error;
-            },
-          } as any),
-      );
-
+      jest.spyOn(jwt, 'verify').mockImplementation(() => {
+        throw new Error('noop');
+      });
       const done = jest.fn();
       await getUserFromToken(token, done);
       expect(done).toHaveBeenCalledTimes(1);
-      expect(done).toHaveBeenCalledWith(error);
+      expect(done.mock.calls[0][0]).toBeInstanceOf(TokenExpiredError);
     });
   });
 
   describe('authenticateUser', () => {
     it('should authenticate correctly if token given', async () => {
-      jest
-        .spyOn(typeorm, 'getRepository')
-        .mockImplementation(
-          () => ({ findOne: jest.fn(() => foundUser) } as any),
-        );
-
       expect(
         await authenticateUser({
           headers: { authorization: 'Bearer example' },
@@ -77,32 +50,14 @@ describe('passport', () => {
       ).toMatchObject(foundUser);
     });
 
-    it('should not authenticate if not found', async () => {
-      jest
-        .spyOn(typeorm, 'getRepository')
-        .mockImplementation(() => ({ findOne: jest.fn(() => null) } as any));
-
-      expect(
-        await authenticateUser({
-          headers: { authorization: 'Bearer example' },
-        } as any),
-      ).toBeNull();
-    });
-
     it('should not authenticate if no token given', async () => {
       expect(await authenticateUser({ headers: {} } as any)).toBeNull();
     });
 
     it('should not authenticate if error', async () => {
-      const error = new Error('noop');
-      jest.spyOn(typeorm, 'getRepository').mockImplementation(
-        () =>
-          ({
-            findOne: async () => {
-              throw error;
-            },
-          } as any),
-      );
+      verify = jest.spyOn(jwt, 'verify').mockImplementation(() => {
+        throw new Error('noop');
+      });
 
       await expect(
         authenticateUser({
