60a28bed929e6e4c23e65916aa65f6dbb9264b4c
Nacho <iaacosta@uc.cl>
2020-08-09 01:51:22 -0400

    improvement(snacks): use page colors and add dismiss button

--- web/src/AppProviders.tsx
+++ web/src/AppProviders.tsx
@@ -1,29 +1,64 @@
-import React from 'react';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { createRef } from 'react';
 import { ApolloProvider } from '@apollo/client';
-import { ThemeProvider, CssBaseline } from '@material-ui/core';
+import { ThemeProvider, CssBaseline, makeStyles, IconButton } from '@material-ui/core';
+import { Close as CloseIcon } from '@material-ui/icons';
 import { SnackbarProvider } from 'notistack';
 import { BrowserRouter as RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import client from './config/apollo';
-import theme from './config/materialUi';
+import customTheme from './config/materialUi';
 import { baseStore } from './config/redux';
 
-const AppProviders: React.FC = ({ children }) => {
+const useSnackbarClasses = makeStyles((theme) => ({
+  variantInfo: { backgroundColor: theme.palette.info.main },
+  variantWarning: { backgroundColor: theme.palette.warning.main },
+  variantError: { backgroundColor: theme.palette.error.main },
+  variantSuccess: { backgroundColor: theme.palette.success.main },
+}));
+
+const useStyles = makeStyles((theme) => ({
+  icon: { color: theme.palette.background.default, marginRight: theme.spacing(1) },
+}));
+
+const CustomSnackbarProvider: React.FC = ({ children }) => {
+  const snackbarClasses = useSnackbarClasses();
+  const classes = useStyles();
+  const notistackRef = createRef<any>();
+
+  const handleDismiss = (key: any) => notistackRef.current.closeSnackbar(key);
+
   return (
-    <RouterProvider>
-      <Provider store={baseStore}>
-        <ApolloProvider client={client}>
-          <ThemeProvider theme={theme}>
-            <SnackbarProvider maxSnack={2} autoHideDuration={2000}>
-              <CssBaseline />
-              {children}
-            </SnackbarProvider>
-          </ThemeProvider>
-        </ApolloProvider>
-      </Provider>
-    </RouterProvider>
+    <SnackbarProvider
+      maxSnack={3}
+      autoHideDuration={4000}
+      classes={snackbarClasses}
+      ref={notistackRef}
+      action={(key) => (
+        <IconButton className={classes.icon} onClick={() => handleDismiss(key)} size="small">
+          <CloseIcon />
+        </IconButton>
+      )}
+    >
+      {children}
+    </SnackbarProvider>
   );
 };
+
+const AppProviders: React.FC = ({ children }) => (
+  <RouterProvider>
+    <Provider store={baseStore}>
+      <ApolloProvider client={client}>
+        <ThemeProvider theme={customTheme}>
+          <CustomSnackbarProvider>
+            <CssBaseline />
+            {children}
+          </CustomSnackbarProvider>
+        </ThemeProvider>
+      </ApolloProvider>
+    </Provider>
+  </RouterProvider>
+);
 
 export default AppProviders;
