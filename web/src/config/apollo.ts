c062927bd818c8afc60ceb75e68cadd7972cb4ce
Nacho <iaacosta@uc.cl>
2020-08-09 01:33:52 -0400

    feat(auth): add messages if tokens expire or user removed

--- web/src/config/apollo.ts
+++ web/src/config/apollo.ts
@@ -1,9 +1,11 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 /* eslint-disable no-console */
 import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { onError } from '@apollo/client/link/error';
 import { capitalize } from 'lodash';
-import { baseStore } from './redux';
+import { baseStore, expireToken } from './redux';
+import { logoutHof } from '../hooks/useLogout';
 
 const baseLink = new HttpLink({
   uri: process.env.REACT_APP_API_URL || 'http://localhost:5000/graphql',
@@ -16,11 +18,16 @@ const contextLink = setContext((_, { headers }) => {
 });
 
 const errorLink = onError(({ graphQLErrors }) => {
-  if (graphQLErrors && process.env.NODE_ENV !== 'production')
+  if (graphQLErrors && process.env.NODE_ENV !== 'production') {
     graphQLErrors.forEach(({ message }, idx) => {
       // eslint-disable-next-line no-param-reassign
       graphQLErrors[idx].message = capitalize(message);
     });
+
+    if (graphQLErrors[0].message.includes('expired')) {
+      logoutHof(expireToken(), baseStore.dispatch, client)();
+    }
+  }
 });
 
 const client = new ApolloClient({
--- web/src/config/redux.ts
+++ web/src/config/redux.ts
@@ -2,20 +2,24 @@ import { createStore } from 'redux';
 
 export const addToken = (token: string) => ({ type: 'ADD_TOKEN', token } as const);
 export const removeToken = () => ({ type: 'REMOVE_TOKEN' } as const);
+export const expireToken = () => ({ type: 'EXPIRE_TOKEN' } as const);
 
-const initialState = {
+export const initialState = {
   token: localStorage.getItem('x-auth') || null,
+  tokenExpired: false,
 };
 
 const baseReducer = (
   state: typeof initialState = initialState,
-  action: ReturnType<typeof addToken | typeof removeToken>,
+  action: ReturnType<typeof addToken | typeof removeToken | typeof expireToken>,
 ) => {
   switch (action.type) {
     case 'ADD_TOKEN':
-      return { ...state, token: action.token };
+      return { ...state, token: action.token, tokenExpired: false };
     case 'REMOVE_TOKEN':
       return { ...state, token: null };
+    case 'EXPIRE_TOKEN':
+      return { ...state, token: null, tokenExpired: true };
     default:
       return state;
   }
