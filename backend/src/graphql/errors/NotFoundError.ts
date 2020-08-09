dcab192cee1d8e842bb90d7c852eb14c7faead14
Nacho <iaacosta@uc.cl>
2020-08-09 01:32:21 -0400

    refactor(errors): add package to get indefinite pronoun

--- backend/src/graphql/errors/NotFoundError.ts
+++ backend/src/graphql/errors/NotFoundError.ts
@@ -1,9 +1,5 @@
 import { UserInputError } from 'apollo-server-express';
-
-export const prependAToResource = (resource: string) => {
-  if (['a', 'e', 'i', 'o', 'u'].includes(resource[0])) return `an ${resource}`;
-  return `a ${resource}`;
-};
+import a from 'indefinite';
 
 export default class NotFoundError extends UserInputError {
   constructor(error: Error) {
@@ -12,9 +8,8 @@ export default class NotFoundError extends UserInputError {
     const matching = error.message.match(/^.* "(.*)" matching.*$/);
     if (matching) resource = matching[1].toLowerCase();
 
-    super(
-      `couldn't find ${prependAToResource(resource)} with such identifier`,
-      { id: 'non existing' },
-    );
+    super(`couldn't find ${a(resource)} with such identifier`, {
+      id: 'non existing',
+    });
   }
 }
