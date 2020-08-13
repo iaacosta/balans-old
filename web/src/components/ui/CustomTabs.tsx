981f6f90650090b167ffa4d6aa44818c321a13f8
Nacho <iaacosta@uc.cl>
2020-08-13 10:36:03 -0400

    refactor(tabs): use custom tab component

---
+++ web/src/components/ui/CustomTabs.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Paper, Tabs, makeStyles, Tab } from '@material-ui/core';
+
+type Props<T> = {
+  tabs: { key: T; label: string }[];
+  selected: T;
+  change: (tab: T) => void;
+};
+
+const useStyles = makeStyles(() => ({
+  paper: { display: 'inline-block', alignSelf: 'flex-start' },
+}));
+
+const CustomTabs = <T extends string>({ tabs, selected, change }: Props<T>): React.ReactElement => {
+  const classes = useStyles();
+
+  return (
+    <Paper square className={classes.paper} elevation={1}>
+      <Tabs textColor="secondary" value={selected} onChange={(event, value) => change(value as T)}>
+        {tabs.map(({ key, label }) => (
+          <Tab value={key} label={label} />
+        ))}
+      </Tabs>
+    </Paper>
+  );
+};
+
+export default CustomTabs;
