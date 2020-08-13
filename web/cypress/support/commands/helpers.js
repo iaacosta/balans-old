2969b662b3cbf1f71531172468a7ffd17dd63b59
Nacho <iaacosta@uc.cl>
2020-08-13 12:42:53 -0400

    improvement(cypress): extends use of change select option
    now accepts labels

--- web/cypress/support/commands/helpers.js
+++ web/cypress/support/commands/helpers.js
@@ -2,7 +2,13 @@
 
 Cypress.Commands.add('submitForm', () => cy.get('button[type="submit"]').click());
 
-Cypress.Commands.add('changeSelectOption', (testId, idx) => {
+Cypress.Commands.add('changeSelectOption', (testId, selector) => {
   cy.findByTestId(testId).click();
-  cy.findByRole('presentation').within(() => cy.get('li').eq(idx).click());
+  cy.findByRole('presentation').within(() => {
+    if (typeof selector === 'number') {
+      cy.get('li').eq(selector).click();
+    } else if (typeof selector === 'string') {
+      cy.get('li').within(() => cy.contains(selector).click());
+    }
+  });
 });
