c1fffaa1445b3acd3cbaa57f6832279de27865f7
Nacho <iaacosta@uc.cl>
2020-08-13 12:43:47 -0400

    test(account): add e2e tests

--- web/cypress/support/matchers.ts
+++ web/cypress/support/matchers.ts
@@ -1,6 +1,7 @@
 export const validationMatchers = {
   requiredField: /required/i,
   minimumLength: /at least/i,
+  minimumNumber: /greater than/i,
   username: /numbers, letters and/i,
   beEmail: /valid email/i,
   confirmPassword: /don't match/i,
