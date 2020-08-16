18af85dac9e20407264f97ef260b3fc8fa535a41
Ignacio Acosta <iaacosta@uc.cl>
2020-08-15 23:51:54 -0400

    style(transaction): change naming on drawer and rules

--- web/src/utils/rbac.ts
+++ web/src/utils/rbac.ts
@@ -8,7 +8,7 @@ export const roles = {
 export const actions = {
   routes: {
     dashboard: 'routes:dashboard',
-    accounts: 'routes:accounts',
+    transactions: 'routes:transactions',
     movements: 'routes:movements',
     otherMovements: 'routes:otherMovements',
     places: 'routes:places',
@@ -31,7 +31,7 @@ export type Action =
 export const rules = {
   user: new Set<string>([
     actions.routes.dashboard,
-    actions.routes.accounts,
+    actions.routes.transactions,
     actions.routes.movements,
     actions.routes.otherMovements,
     actions.routes.people,
@@ -39,7 +39,7 @@ export const rules = {
   ]),
   admin: new Set<string>([
     actions.routes.dashboard,
-    actions.routes.accounts,
+    actions.routes.transactions,
     actions.routes.movements,
     actions.routes.otherMovements,
     actions.routes.people,
