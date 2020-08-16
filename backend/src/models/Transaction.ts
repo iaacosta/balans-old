82374fb80795f55236807feaf006ca4542d5b207
Ignacio Acosta <iaacosta@uc.cl>
2020-08-15 22:53:57 -0400

    feat(transaction): add create transaction resolver and type definition

--- backend/src/models/Transaction.ts
+++ backend/src/models/Transaction.ts
@@ -7,31 +7,42 @@ import {
   UpdateDateColumn,
   DeleteDateColumn,
 } from 'typeorm';
+import { ObjectType, Field, ID, Int } from 'type-graphql';
+import { NotEquals } from 'class-validator';
 import Account from './Account';
 
+@ObjectType()
 @Entity()
 export default class Transaction {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(() => Int)
   @Column('integer')
+  @NotEquals(0)
   amount: number;
 
   @Column()
   accountId: number;
 
+  @Field(() => Int)
   @Column()
   resultantBalance: number;
 
+  @Field(() => Account)
   @ManyToOne(() => Account, { onDelete: 'CASCADE' })
   account: Account;
 
+  @Field()
   @CreateDateColumn()
   createdAt: Date;
 
+  @Field()
   @UpdateDateColumn()
   updatedAt: Date;
 
+  @Field({ nullable: true })
   @DeleteDateColumn()
   deletedAt: Date;
 
