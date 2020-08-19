3a0734f63f0a737f4a30171b06bdfb0735bf7a8f
Ignacio Acosta <iaacosta@uc.cl>
2020-08-18 23:05:43 -0400

    refactor(error): uses resource as NotFoundError constructor param instead of error

--- backend/src/graphql/errors/NotFoundError.ts
+++ backend/src/graphql/errors/NotFoundError.ts
@@ -2,13 +2,8 @@ import { UserInputError } from 'apollo-server-express';
 import a from 'indefinite';
 
 export default class NotFoundError extends UserInputError {
-  constructor(error: Error) {
-    let resource = 'resource';
-
-    const matching = error.message.match(/^.* "(.*)" matching.*$/);
-    if (matching) resource = matching[1].toLowerCase();
-
-    super(`couldn't find ${a(resource)} with such identifier`, {
+  constructor(resource?: string) {
+    super(`couldn't find ${a(resource || 'resource')} with such identifier`, {
       id: 'non existing',
     });
   }
