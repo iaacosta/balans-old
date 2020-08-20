fabf8d29cccacb8ae625c62ec8e3dd6a85e2f7a6
Nacho <iaacosta@uc.cl>
2020-08-19 23:13:39 -0400

    improvement(transaction): disable transaction creation if no accounts created

--- web/src/pages/Transactions.tsx
+++ web/src/pages/Transactions.tsx
@@ -6,6 +6,9 @@ import ViewportContainer from '../components/ui/ViewportContainer';
 import DialogButton from '../components/ui/DialogButton';
 import CreateTransactionDialog from '../components/transactions/CreateTransactionDialog';
 import TransactionsTable from '../components/transactions/TransactionsTable';
+import { useRedirectedQuery } from '../hooks/useRedirectedQuery';
+import { myAccountsQuery } from '../graphql/account';
+import { MyAccountsQuery } from '../@types/graphql';
 
 const useStyles = makeStyles((theme) => ({
   title: { marginBottom: theme.spacing(2) },
@@ -13,14 +16,17 @@ const useStyles = makeStyles((theme) => ({
 
 const Transactions: React.FC = () => {
   const classes = useStyles();
+  const { data, loading } = useRedirectedQuery<MyAccountsQuery>(myAccountsQuery);
 
+  const noAccountsCreated = !!data && data.accounts.length === 0;
   return (
     <ViewportContainer>
       <Typography className={classes.title} variant="h5">
         My transactions
       </Typography>
-      <TransactionsTable>
+      <TransactionsTable accountsLoading={loading || !data} noAccountsCreated={noAccountsCreated}>
         <DialogButton
+          disabled={loading || noAccountsCreated}
           buttonLabel="Add new transaction"
           DialogComponent={CreateTransactionDialog}
           data-testid="createTransactionButton"
