0639b40e77e3106773b69e146fc1f9c58baac43d
Nacho <iaacosta@uc.cl>
2020-08-22 12:49:49 -0400

    improvement(ui): adds responsive dialog

---
+++ web/src/components/ui/ResponsiveDialog.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Dialog, DialogProps, makeStyles } from '@material-ui/core';
+import { useBreakpoint } from '../../hooks/useBreakpoint';
+
+const useStyles = makeStyles(() => ({
+  paperFullScreen: { justifyContent: 'center' },
+}));
+
+const ResponsiveDialog: React.FC<DialogProps> = ({ children, maxWidth, classes, ...props }) => {
+  const customClasses = useStyles();
+  const isMobile = useBreakpoint({ layout: 'xs' });
+  return (
+    <Dialog
+      {...props}
+      classes={{ ...customClasses, ...classes }}
+      fullScreen={isMobile}
+      hideBackdrop={isMobile}
+    >
+      {children}
+    </Dialog>
+  );
+};
+
+export default ResponsiveDialog;
