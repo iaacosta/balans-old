a3f96526e7117aff743566ea71910060d7493195
Nacho <iaacosta@uc.cl>
2020-08-23 18:47:19 -0400

    wip(ui): add redux slice on theming

--- web/src/config/redux.ts
+++ web/src/config/redux.ts
@@ -1,13 +1,16 @@
 /* eslint-disable no-param-reassign */
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer, { authInitialState } from '../slices/authSlice';
+import themeReducer, { themeInitialState } from '../slices/themeSlice';
 
 export type AppState = {
   auth: typeof authInitialState;
+  theme: typeof themeInitialState;
 };
 
 export const store = configureStore({
   reducer: combineReducers({
     auth: authReducer,
+    theme: themeReducer,
   }),
 });
