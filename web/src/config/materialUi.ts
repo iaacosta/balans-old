8d97436ae1673df9bd5c0b8e4ae276c345503b29
Nacho <iaacosta@uc.cl>
2020-08-23 18:47:31 -0400

    wip(ui): creates new mui theme

--- web/src/config/materialUi.ts
+++ web/src/config/materialUi.ts
@@ -1,13 +1,32 @@
 import { createMuiTheme, responsiveFontSizes } from '@material-ui/core';
+import { ResponsiveFontSizesOptions } from '@material-ui/core/styles/responsiveFontSizes';
 
-const theme = createMuiTheme({
-  palette: {
-    primary: { main: '#6D4482' },
-    secondary: { main: '#009FB7' },
-    success: { main: '#5CAD68', contrastText: '#fff' },
-    error: { main: '#D63230' },
-    warning: { main: '#F49D37' },
-  },
-});
+const responsiveFontOptions: ResponsiveFontSizesOptions = { factor: 3 };
 
-export default responsiveFontSizes(theme, { factor: 3 });
+export const lightTheme = responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      primary: { main: '#6D4482' },
+      secondary: { main: '#009FB7' },
+      success: { main: '#5CAD68', contrastText: '#fff' },
+      error: { main: '#D63230' },
+      warning: { main: '#F49D37' },
+    },
+  }),
+  responsiveFontOptions,
+);
+
+export const darkTheme = responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      type: 'dark',
+      primary: { main: '#222831' },
+      secondary: { main: '#00B8AB' },
+      success: { main: '#77BB81' },
+      info: { main: '#4f9ccf' },
+      error: { main: '#ec625f' },
+      warning: { main: '#ffc045' },
+    },
+  }),
+  responsiveFontOptions,
+);
