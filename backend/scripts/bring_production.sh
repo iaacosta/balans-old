a99c32df18d517bfd68cf425b17d072613731fbe
Ignacio Acosta <iaacosta@uc.cl>
2020-08-25 15:29:27 -0400

    chore(scripts): add script to sync production database

---
+++ backend/scripts/bring_production.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# TODO: set it up with docker
+
+# dump database
+pg_dump -Fc -f rds.bak postgres://$RDS_USERNAME:$RDS_PASSWORD@$RDS_HOSTNAME:5432/$RDS_NAME
+# restore local database
+pg_restore -c -U $DB_USERNAME -h $DB_HOSTNAME -d $DB_NAME -Fc --no-acl rds.bak
+# clean file
+rm rds.bak
+
+
