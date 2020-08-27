4babf151777b935ff97cc1d967b73e08485a0ac9
Ignacio Acosta <iaacosta@uc.cl>
2020-08-27 14:39:28 -0400

    wip(apollo): adds helper hook to handle mutations with unique input variable

--- web/src/@types/helpers.ts
+++ web/src/@types/helpers.ts
@@ -1,3 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { MutationTuple } from '@apollo/client';
+
 export type ElementOf<T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer ElementOf>
   ? ElementOf
   : never;
+
+export type InputMutationTuple<TData, TVariables extends { input: any }> = [
+  (input: TVariables['input']) => ReturnType<MutationTuple<TData, TVariables>[0]>,
+  MutationTuple<TData, TVariables>[1],
+];
