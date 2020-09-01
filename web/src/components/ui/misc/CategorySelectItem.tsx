9ee2a6ae84941f5d37f5955786a564d48c0f1f6d
Ignacio Acosta <iaacosta@uc.cl>
2020-08-31 22:46:21 -0400

    fix(ui): add responsiveness to categories page

--- web/src/components/ui/misc/CategorySelectItem.tsx
+++ web/src/components/ui/misc/CategorySelectItem.tsx
@@ -8,10 +8,12 @@ const useStyles = makeStyles((theme) => ({
   wrapper: {
     display: 'flex',
     alignItems: 'center',
+    minWidth: 0,
     '& > *:not(:last-child)': {
       marginRight: theme.spacing(1),
     },
   },
+  text: { textOverflow: 'ellipsis', whiteSpace: 'nowrap', overflow: 'hidden' },
 }));
 
 type Props = { category: MyCategoriesQuery['income'][number]; className?: string };
@@ -21,7 +23,9 @@ const CategorySelectItem: React.FC<Props> = ({ category: { name, color }, classN
   return (
     <Box className={clsx(classes.wrapper, className)} display="flex" alignItems="center">
       <CategoryIcon color={color} />
-      <Typography variant="body1">{name}</Typography>
+      <Typography className={classes.text} variant="body1">
+        {name}
+      </Typography>
     </Box>
   );
 };
