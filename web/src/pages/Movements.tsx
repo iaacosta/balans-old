8283406d5548d9cba0d4a5d71f8a8cdd6155f8d6
Ignacio Acosta <iaacosta@uc.cl>
2020-09-01 22:51:40 -0400

    refactor(transaction): move table/list logic to encapsulated component

--- web/src/pages/Movements.tsx
+++ web/src/pages/Movements.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Typography, makeStyles, Hidden, Box } from '@material-ui/core';
-import { Add as AddIcon } from '@material-ui/icons';
+import { Typography, makeStyles } from '@material-ui/core';
 import ViewportContainer from '../components/ui/misc/ViewportContainer';
-import { useMyDebitAccounts, useMyTransactions } from '../hooks/graphql';
-import TransactionsTable from '../components/transactions/TransactionsTable';
-import TransactionsList from '../components/transactions/TransactionsList';
-import CreateTransactionDialog from '../components/transactions/CreateTransactionDialog';
-import DialogButton from '../components/ui/dialogs/DialogButton';
-import { useMyCategories } from '../hooks/graphql/category';
 import CustomTabs from '../components/ui/navigation/CustomTabs';
 import { useTabs } from '../hooks/utils/useTabs';
+import Transactions from '../components/transactions/Transactions';
 
 const useStyles = makeStyles((theme) => ({
   title: { marginBottom: theme.spacing(2) },
-  buttonWrapper: {
-    display: 'flex',
-    justifyContent: 'flex-end',
-    marginTop: theme.spacing(2),
-  },
 }));
 
 const tabs = [
@@ -28,27 +17,10 @@ const tabs = [
 
 const Movements: React.FC = () => {
   const classes = useStyles();
-  const { transactions, loading: transactionsLoading } = useMyTransactions();
-  const { accounts, loading: accountsLoading } = useMyDebitAccounts();
-  const { loading: loadingCategory } = useMyCategories();
   const { selected, change } = useTabs({
     tabs: tabs.map(({ key }) => key),
     initialTab: 'transactions',
   });
-
-  const noAccounts = accounts.length === 0;
-  const loading = transactionsLoading || accountsLoading || loadingCategory;
-
-  const Button = (
-    <DialogButton
-      disabled={loading || noAccounts}
-      buttonLabel="Add new transaction"
-      data-testid="createTransactionButton"
-      startIcon={<AddIcon />}
-    >
-      <CreateTransactionDialog />
-    </DialogButton>
-  );
 
   return (
     <ViewportContainer>
@@ -56,27 +28,7 @@ const Movements: React.FC = () => {
         My movements
       </Typography>
       <CustomTabs tabs={tabs} selected={selected} change={change} />
-      {selected === 'transactions' && (
-        <>
-          <Hidden smDown>
-            <TransactionsTable
-              transactions={transactions}
-              loading={loading}
-              noAccountsCreated={noAccounts}
-            >
-              {Button}
-            </TransactionsTable>
-          </Hidden>
-          <Hidden mdUp>
-            <TransactionsList
-              transactions={transactions}
-              loading={loading}
-              noAccountsCreated={noAccounts}
-            />
-            <Box className={classes.buttonWrapper}>{Button}</Box>
-          </Hidden>
-        </>
-      )}
+      {selected === 'transactions' && <Transactions />}
     </ViewportContainer>
   );
 };
