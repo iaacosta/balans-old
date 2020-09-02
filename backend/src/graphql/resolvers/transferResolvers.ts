f340315ff384a2b162b18182e7970af2170e3e3a
Ignacio Acosta <iaacosta@uc.cl>
2020-09-02 08:57:55 -0400

    feat(transfer): add resolver to get my transfers

--- backend/src/graphql/resolvers/transferResolvers.ts
+++ backend/src/graphql/resolvers/transferResolvers.ts
@@ -6,8 +6,12 @@ import {
   Authorized,
   FieldResolver,
   Root,
+  Query,
+  ObjectType,
+  Field,
 } from 'type-graphql';
 import { EntityManager, getConnection } from 'typeorm';
+import { groupBy, map } from 'lodash';
 
 import Transfer from '../../models/Transfer';
 import { CreateTransferInput } from '../helpers';
@@ -15,12 +19,56 @@ import { Context } from '../../@types';
 import Account from '../../models/Account';
 import TransferCommands from '../../commands/TransferCommands';
 
+@ObjectType()
+class PairedTransfer {
+  @Field(() => Transfer)
+  from: Transfer;
+
+  @Field(() => Transfer)
+  to: Transfer;
+}
+
 @Resolver(Transfer)
 export default class TransferResolvers {
   manager: EntityManager;
 
   constructor() {
     this.manager = getConnection().createEntityManager();
+  }
+
+  @Query(() => [Transfer])
+  @Authorized()
+  async myTransfers(@Ctx() { currentUser }: Context): Promise<Transfer[]> {
+    return this.manager
+      .getRepository(Transfer)
+      .createQueryBuilder('transfer')
+      .select()
+      .leftJoin('transfer.account', 'account')
+      .where('account.userId = :userId', { userId: currentUser!.id })
+      .orderBy('transfer.createdAt', 'DESC')
+      .addOrderBy('transfer.amount', 'ASC')
+      .getMany();
+  }
+
+  @Query(() => [PairedTransfer])
+  @Authorized()
+  async myPairedTransfers(
+    @Ctx() { currentUser }: Context,
+  ): Promise<PairedTransfer[]> {
+    const transfers = await this.manager
+      .getRepository(Transfer)
+      .createQueryBuilder('transfer')
+      .select()
+      .leftJoin('transfer.account', 'account')
+      .where('account.userId = :userId', { userId: currentUser!.id })
+      .orderBy('transfer.createdAt', 'DESC')
+      .addOrderBy('transfer.amount', 'ASC')
+      .getMany();
+
+    return map(groupBy(transfers, 'operationId'), ([from, to]) => ({
+      from,
+      to,
+    }));
   }
 
   @Mutation(() => [Transfer])
