f8b10c5e82cc484fa64a12759cca44bbe631a07e
Ignacio Acosta <iaacosta@uc.cl>
2020-09-02 16:56:53 -0400

    wip(transfer): removes resultant balance from all transactions
    this because it turns out rather difficult to calculate it when not ALL movements are at hand, it makes more sense that it is implemented in the account profile

--- web/src/components/transactions/TransactionsTable.tsx
+++ web/src/components/transactions/TransactionsTable.tsx
@@ -3,12 +3,7 @@ import { makeStyles, Typography, Box } from '@material-ui/core';
 import { Column } from 'react-table';
 import { formatMoney } from 'accounting';
 import dayjs from 'dayjs';
-import {
-  ArrowDropDown as ArrowDropDownIcon,
-  ArrowDropUp as ArrowDropUpIcon,
-} from '@material-ui/icons';
 import clsx from 'clsx';
-import { groupBy, mapValues } from 'lodash';
 import { MyTransactionsQuery } from '../../@types/graphql';
 import EnhancedTable from '../ui/dataDisplay/EnhancedTable';
 import TransactionActionCell from './TransactionActionCell';
@@ -31,10 +26,6 @@ type Props = {
   noAccountsCreated: boolean;
 };
 
-export type ProcessedTransaction = MyTransactionsQuery['transactions'][number] & {
-  resultantBalance: number;
-};
-
 const TransactionsTable: React.FC<Props> = ({
   children,
   transactions,
@@ -43,32 +34,19 @@ const TransactionsTable: React.FC<Props> = ({
 }) => {
   const classes = useStyles();
 
-  /* TODO: lookup how to display loading if computing this */
-  const processedTransactions = useMemo(() => {
-    const withRunningBalance: ProcessedTransaction[] = [];
-    const balances: { [key: string]: number } = mapValues(
-      groupBy(transactions, (transaction) => transaction.account.id),
-      (groupedTransactions) => groupedTransactions.reduce((accum, curr) => accum + curr.amount, 0),
-    );
-
-    transactions.forEach((transaction) => {
-      withRunningBalance.push({
-        ...transaction,
-        resultantBalance: balances[transaction.account.id],
-      });
-
-      balances[transaction.account.id] -= transaction.amount;
-    });
-
-    return withRunningBalance;
-  }, [transactions]);
-
-  const columns: Column<ProcessedTransaction>[] = useMemo(
+  const columns: Column<MyTransactionsQuery['transactions'][number]>[] = useMemo(
     () => [
       {
         Header: 'Amount',
         accessor: 'amount',
-        Cell: ({ value }) => formatMoney(value),
+        Cell: ({ value }) => (
+          <Typography
+            className={clsx(value < 0 && classes.expense, value > 0 && classes.income)}
+            variant="body2"
+          >
+            {formatMoney(value)}
+          </Typography>
+        ),
       },
       {
         Header: 'Account',
@@ -79,29 +57,6 @@ const TransactionsTable: React.FC<Props> = ({
         Header: 'Memo',
         accessor: 'memo',
         Cell: ({ value }) => value || '-',
-      },
-      {
-        Header: 'Resultant balance',
-        accessor: 'resultantBalance',
-        Cell: ({ value, row }) => {
-          const cls = clsx(
-            row.original.amount < 0 && classes.expense,
-            row.original.amount > 0 && classes.income,
-          );
-
-          return (
-            <Box display="flex" alignItems="center">
-              {row.original.amount > 0 ? (
-                <ArrowDropUpIcon className={cls} fontSize="small" />
-              ) : (
-                <ArrowDropDownIcon className={cls} fontSize="small" />
-              )}
-              <Typography variant="body2" className={cls}>
-                {formatMoney(value)}
-              </Typography>
-            </Box>
-          );
-        },
       },
       {
         Header: 'Category',
@@ -132,7 +87,7 @@ const TransactionsTable: React.FC<Props> = ({
       className={classes.table}
       loading={loading}
       columns={columns}
-      data={processedTransactions}
+      data={transactions}
       initialState={{ pageSize: 7 }}
       noEntriesLabel={
         noAccountsCreated
