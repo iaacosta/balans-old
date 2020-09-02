49c73401b7969290df694d5ea7c275c1098429be
Ignacio Acosta <iaacosta@uc.cl>
2020-09-01 20:24:05 -0400

    feat(transfer): add create transfer mutation

---
+++ backend/src/graphql/helpers/inputs/transferInputs.ts
@@ -0,0 +1,16 @@
+import { InputType, Field, Int, ID } from 'type-graphql';
+
+@InputType()
+export class CreateTransferInput {
+  @Field(() => Int)
+  amount: number;
+
+  @Field(() => ID)
+  fromAccountId: number;
+
+  @Field(() => ID)
+  toAccountId: number;
+
+  @Field({ nullable: true })
+  memo?: string;
+}
