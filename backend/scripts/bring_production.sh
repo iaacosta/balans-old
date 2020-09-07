fc268d4f69e89d4480a8a8a8e8a199b08b81f4a7
Nacho <iaacosta@uc.cl>
2020-09-07 11:38:22 -0300

    chore: add database clean to sync database script

--- backend/scripts/bring_production.sh
+++ backend/scripts/bring_production.sh
@@ -2,6 +2,8 @@
 
 # TODO: set it up with docker
 
+# clean database
+yarn typeorm-cli schema:drop
 # dump database
 pg_dump -Fc -f rds.bak postgres://$RDS_USERNAME:$RDS_PASSWORD@$RDS_HOSTNAME:5432/$RDS_NAME
 # restore local database
