616bde2cd8121927dab8ec7f5b4e367fee3648ab
Nacho <iaacosta@uc.cl>
2020-11-07 23:33:19 -0300

    chore(yup): add custom message for max dates

--- web/src/config/yup.ts
+++ web/src/config/yup.ts
@@ -1,8 +1,10 @@
 /* eslint-disable no-template-curly-in-string */
 import { setLocale } from 'yup';
+import { longDateFormatter } from '../utils/date';
 
 setLocale({
   mixed: { required: 'This field is required' },
   string: { email: 'Invalid email', min: 'Must be at least ${min} characters long' },
   number: { min: ({ min }) => `Must be greater than ${min - 1}` },
+  date: { max: ({ max }) => `Must be earlier than ${longDateFormatter(max)}` },
 });
