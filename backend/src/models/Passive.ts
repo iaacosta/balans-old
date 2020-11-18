a69492bcf08c5901630b5532ab71fc1a41f0ac97
Nacho <iaacosta@uc.cl>
2020-11-18 17:11:22 -0300

    feat(backend): add liquidated status to passives
    model changes + migration

--- backend/src/models/Passive.ts
+++ backend/src/models/Passive.ts
@@ -1,7 +1,30 @@
-import { Entity } from 'typeorm';
-import { ObjectType } from 'type-graphql';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { Field, ObjectType } from 'type-graphql';
 import Movement from './Movement';
+import Account from './Account';
 
 @Entity()
 @ObjectType()
-export default class Passive extends Movement {}
+export default class Passive extends Movement {
+  @Column()
+  @Field()
+  liquidated: boolean;
+
+  @Column({ nullable: true })
+  liquidatedAccountId?: number;
+
+  @Field(() => Account, { nullable: true })
+  @ManyToOne(() => Account, { eager: false, onDelete: 'CASCADE' })
+  liquidatedAccount?: Account;
+
+  constructor(passive: {
+    amount: number;
+    accountId: number;
+    memo?: string;
+    issuedAt?: Date;
+    operationId?: string;
+  }) {
+    super(passive);
+    if (passive) this.liquidated = false;
+  }
+}
