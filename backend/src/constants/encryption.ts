777ed2db9242e471f88170eb5a113a26cb7acfe4
Nacho <iaacosta@uc.cl>
2020-12-06 19:26:05 -0300

    feat(backend): add encrypt/decrypt logic to crypto util

---
+++ backend/src/constants/encryption.ts
@@ -0,0 +1,5 @@
+import { CipherGCMTypes } from 'crypto';
+
+export default {
+  cipherAlgorithm: 'aes-192-gcm' as CipherGCMTypes,
+};
--- backend/src/utils/CryptoUtil.ts
+++ backend/src/utils/CryptoUtil.ts
@@ -1,4 +1,18 @@
+import {
+  scrypt as callbackScrypt,
+  createCipheriv,
+  createDecipheriv,
+} from 'crypto';
 import { compare, genSalt, hash } from 'bcrypt';
+import { promisify } from 'util';
+import encryption from '../constants/encryption';
+
+const scrypt = promisify<string, string, number, Buffer>(callbackScrypt);
+
+export type EncryptionData = {
+  hexInitVector: string;
+  batch: string[];
+};
 
 export type VerifyHashData = {
   data: string;
@@ -6,6 +20,50 @@ export type VerifyHashData = {
 };
 
 export class CryptoUtil {
+  static async encryptData({
+    hexInitVector,
+    batch,
+  }: EncryptionData): Promise<string[]> {
+    const cipherKey = await CryptoUtil.getCipherKey();
+    const initVector = Buffer.from(hexInitVector, 'hex');
+
+    return batch.map((credential) => {
+      const cipher = createCipheriv(
+        encryption.cipherAlgorithm,
+        cipherKey,
+        initVector,
+      );
+
+      const encrypted =
+        cipher.update(credential, 'utf8', 'hex') + cipher.final('hex');
+
+      return `${cipher.getAuthTag().toString('hex')}${encrypted}`;
+    });
+  }
+
+  static async decryptData({
+    batch,
+    hexInitVector,
+  }: EncryptionData): Promise<string[]> {
+    const cipherKey = await CryptoUtil.getCipherKey();
+    const initVector = Buffer.from(hexInitVector, 'hex');
+
+    return batch.map((credential) => {
+      const decipher = createDecipheriv(
+        encryption.cipherAlgorithm,
+        cipherKey,
+        initVector,
+      );
+
+      decipher.setAuthTag(Buffer.from(credential.slice(0, 32), 'hex'));
+      const decrypted =
+        decipher.update(credential.slice(32), 'hex', 'utf8') +
+        decipher.final('utf8');
+
+      return decrypted;
+    });
+  }
+
   static async hashData(data: string): Promise<string> {
     const salt = await genSalt(10);
     return hash(data, salt);
@@ -18,5 +76,10 @@ export class CryptoUtil {
     } catch (err) {
       return false;
     }
+  }
+
+  static getCipherKey(): Promise<Buffer> {
+    const cipherPassword = process.env.CIPHER_PASSWORD!;
+    return scrypt(cipherPassword, 'salt', 24);
   }
 }
