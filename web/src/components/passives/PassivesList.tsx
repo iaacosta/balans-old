af2924537fb9a8f98750c18f0fec4f48f8299e07
Nacho <iaacosta@uc.cl>
2021-01-24 21:06:39 -0300

    improvement(passives) use custom component to show passive details

--- web/src/components/passives/PassivesList.tsx
+++ web/src/components/passives/PassivesList.tsx
@@ -1,23 +1,10 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import React from 'react';
-import {
-  ListItem,
-  ListItemText,
-  makeStyles,
-  Divider,
-  ListItemSecondaryAction,
-  Box,
-  Paper,
-} from '@material-ui/core';
-import { Delete as DeleteIcon, Edit as EditIcon, Payment as PaymentIcon } from '@material-ui/icons';
+import { makeStyles, Box, Paper, useTheme } from '@material-ui/core';
 import { MyPassivesQuery } from '../../@types/graphql';
-import EnhancedIconButton from '../ui/misc/EnhancedIconButton';
 import VirtualizedList from '../ui/dataDisplay/VirtualizedList';
-import DialogIconButton from '../ui/dialogs/DialogIconButton';
 import { useLocale } from '../../hooks/utils/useLocale';
-import LiquidatePassiveDialog from './LiquidatePassiveDialog';
-import AmountTypography from '../ui/dataDisplay/AmountTypography';
-import { useDeletePassive } from '../../hooks/graphql';
+import PassiveListItem from './PassiveListItem';
 
 type Props = {
   passives: MyPassivesQuery['passives'];
@@ -38,63 +25,26 @@ const useStyles = makeStyles((theme) => ({
 
 const PassivesList: React.FC<Props> = ({ passives, loading, noAccountsCreated }) => {
   const { locale } = useLocale();
-  const [deletePassive, { loading: deleteLoading }] = useDeletePassive();
   const classes = useStyles();
+  const theme = useTheme();
 
   return (
     <Paper className={classes.paper} elevation={1}>
       <VirtualizedList
         data={passives}
         loading={loading}
+        customItemHeight={theme.spacing(15)}
         noEntriesLabel={
           noAccountsCreated
             ? locale('movements:atLeastOneAccount')
             : locale('movements:noneCreated', { value: locale('elements:plural:passive') })
         }
       >
-        {({ data, index, style }) => {
-          const { id, amount, account, liquidated, memo } = data[index];
-          return (
-            <Box style={style} key={index}>
-              <ListItem classes={{ container: classes.container }} component="div">
-                <ListItemText
-                  primary={<AmountTypography>{amount}</AmountTypography>}
-                  secondary={`${memo} | ${account.name}`}
-                />
-                <ListItemSecondaryAction className={classes.secondaryActions}>
-                  {!liquidated && (
-                    <DialogIconButton
-                      contained
-                      data-testid={`liquidatePassive${id}`}
-                      color="warning"
-                      icon={<PaymentIcon />}
-                    >
-                      <LiquidatePassiveDialog passive={data[index]} />
-                    </DialogIconButton>
-                  )}
-                  <EnhancedIconButton
-                    contained
-                    disabled
-                    data-testid={`updatePassive${id}`}
-                    color="info"
-                  >
-                    <EditIcon />
-                  </EnhancedIconButton>
-                  <EnhancedIconButton
-                    contained
-                    disabled={deleteLoading}
-                    data-testid={`deletePassive${id}`}
-                    color="error"
-                    onClick={() => deletePassive(id)}
-                  >
-                    <DeleteIcon fontSize="small" />
-                  </EnhancedIconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-              <Divider variant="middle" />
-            </Box>
-          );
-        }}
+        {({ data, index, style }) => (
+          <Box style={style} key={index}>
+            <PassiveListItem passive={data[index]} />
+          </Box>
+        )}
       </VirtualizedList>
     </Paper>
   );
