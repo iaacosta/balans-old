99e7147e57a2cc5e36f98ae7654326bdfacc931e
Ignacio Acosta <iaacosta@uc.cl>
2021-04-03 14:20:54 -0300

    refactor(app): move static file management to own module

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,10 @@
 import express from 'express';
-import { join } from 'path';
 import { createConnection } from 'typeorm';
-
 import { mountApollo } from './config/apollo';
+import { mountStaticFiles } from './config/static';
 
 const { PORT, NODE_ENV } = process.env;
 const app = express();
-
-app.use(express.static(join(__dirname, 'public')));
-app.get('*', (req, res) => res.sendFile(join(__dirname, 'public', 'index.html')));
 
 /* istanbul ignore next */
 if (NODE_ENV !== 'test') {
@@ -16,6 +12,7 @@ if (NODE_ENV !== 'test') {
     .then(async () => {
       console.log('TypeORM connected to Postgres database');
       const server = await mountApollo();
+      mountStaticFiles(app);
       server.applyMiddleware({ app });
       app.listen(PORT, () =>
         console.log(`Express app running on port ${PORT}`),
